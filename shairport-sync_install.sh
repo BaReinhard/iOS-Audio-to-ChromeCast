31003c1a59a8bab97b93514d3e1bdf1914acac93
Brett Reinhard <brett@reinhards.us>
2017-03-14 08:13:55 -0700

    Update shairport-sync_install.sh

--- shairport-sync_install.sh
+++ shairport-sync_install.sh
@@ -4,7 +4,7 @@ read -p "What is your User Name?: " UserName
 
 apt-get install shairport-sync
 systemctl disable shairport-sync
-cp usr/local/bin/shairport-start
+cp usr/local/bin/shairport-start /usr/local/bin/shairport-start
 chmod +x /usr/local/bin/shairport-start
 sed -i -e "\$i \su ${UserName} - -c /usr/local/bin/shairport-start&\n" /etc/rc.local
 
