60be90c6b625ceeddc8a8e38a5b1eedd63bd9780
Brett Reinhard <brett@reinhards.us>
2017-03-14 07:59:49 -0700

    Update shairport-sync_install.sh

--- shairport-sync_install.sh
+++ shairport-sync_install.sh
@@ -6,6 +6,6 @@ apt-get install shairport-sync
 systemctl disable shairport-sync
 cp usr/local/bin/shairport-start
 chmod +x /usr/local/bin/shairport-start
-sed -i -e "$i \su ${UserName} - -c /usr/local/bin/shairport-start& &\n" /etc/rc.local
+sed -i -e "\$i \su ${UserName} - -c /usr/local/bin/shairport-start&\n" /etc/rc.local
 
 exit 0
