528d10cc43400f5532d39bf0830f5d49471c2d6b
Brett Reinhard <brett@reinhards.us>
2017-03-14 08:10:28 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+log="./install.log"
+echo "" > $log
 
 while [ $UserNameAnswer != "y" ];
 do
@@ -6,4 +8,24 @@ do
   echo "User name is : ${UserName}"
   read -p "Is this correct? (y/n) : " UserNameAnswer
 done
+#============================== Error Handler ==========================
+function excmd {
+  echo "==================> Executing: $*"
+  if ! $*; then
+    echo "Exiting script due to error from: $*"
+    exit 1
+  fi
+}
+#============================== End Error Handler ======================
+echo " =========================== Starting Prep Work ======================="
+excmd prep_install.sh | tee -a $log
+echo " ======================================================================"
+echo " ====================== Installing Pulseaudio-dlna ===================="
+excmd pa_dlna_install.sh | tee -a $log
+echo " ======================================================================"
+echo " ====================== Installing Shairport-Sync ====================="
+echo "${UserName}" | excmd shairport-sync_install.sh | tee -a $log
+echo " ======================================================================"
+
+
 
