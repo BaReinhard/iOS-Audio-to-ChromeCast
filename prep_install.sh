289da8c4144244c7721a8454c08a22cf493baba5
Brett Reinhard <brett@reinhards.us>
2017-03-15 06:49:11 -0700

    Update prep_install.sh

--- prep_install.sh
+++ prep_install.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+#============================== Error Handler ==========================
+function excmd {
+  echo "==================> Executing: $*"
+  if ! $*; then
+    echo "Exiting script due to error from: $*"
+    exit 1
+  fi
+}
+#============================== End Error Handler ======================
+
 apt-add-repository ppa:qos/pulseaudio-dlna
 apt-add-repository ppa:dantheperson/shairplay-sync
 apt-get update -y
 apt-get upgrade -y
 echo "" | tee -a /etc/asound.conf
-cp /etc/asound.conf /etc/asound.conf.old
-cp etc/asound.conf /etc/asound.conf
+excmd cp /etc/asound.conf /etc/asound.conf.old
+excmd cp etc/asound.conf /etc/asound.conf
 
 exit 0
