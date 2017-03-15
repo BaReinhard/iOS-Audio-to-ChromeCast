b49e1e9f8b4f3b612bef1351972fd992b9d0dcc4
Brett Reinhard <brett@reinhards.us>
2017-03-15 15:39:34 -0700

    Update shairport-sync_install.sh

--- shairport-sync_install.sh
+++ shairport-sync_install.sh
@@ -38,16 +38,16 @@ excmd cp usr/local/bin/setdefaultsink /usr/local/bin/setdefaultsink
 excmd chmod +x /usr/local/bin/setdefaultsink
 
 # Inserts a line to /etc/rc.local to allow shairport-sync to execute as a non root user
-excmd sed -i -e "\$i \su ${UserName} - -c /usr/local/bin/shairport-start&\n" /etc/rc.local
+sed -i "\$i \su ${UserName} - -c /usr/local/bin/shairport-start&\n" /etc/rc.local
 
 # Inserts the Chosen AirPlay name into shairport-sync.conf at line 7
-excmd sed -i -e "7i \name = "${AirPlay}";\n" /etc/shairport-sync.conf
+sed -i "7i \name = \"${AirPlay}\";" /etc/shairport-sync.conf
 
 # Inserts autostream line into shairport-syncs auto caller on connection of AirPlay
-excmd sed -i -e '53i \run_this_before_play_begins = "/usr/local/bin/autostream";\n' /etc/shairport-sync.conf
+sed -i '53i \run_this_before_play_begins = "/usr/local/bin/autostream";\n' /etc/shairport-sync.conf
 
 # Insets wait for completion so the script won't block shairport-sync from adding the input stream
-excmd sed -i -e '53i \wait_for_completion = "no";' /etc/shairport-sync.conf
+sed -i '53i \wait_for_completion = "no";' /etc/shairport-sync.conf
 
 # Added shairport-sync to the pulse and lp group
 excmd usermod -a -G pulse shairport-sync
