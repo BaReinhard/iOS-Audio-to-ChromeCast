b2213bccc0350917f041d6b531950394f2209dab
Brett Reinhard <brett@reinhards.us>
2017-03-15 06:50:55 -0700

    Update shairport-sync_install.sh

--- shairport-sync_install.sh
+++ shairport-sync_install.sh
@@ -2,11 +2,22 @@
 
 read -p "What is your User Name?: " UserName
 
+#============================== Error Handler ==========================
+function excmd {
+  echo "==================> Executing: $*"
+  if ! $*; then
+    echo "Exiting script due to error from: $*"
+    exit 1
+  fi
+}
+#============================== End Error Handler ======================
+
+
 apt-get install shairport-sync
-systemctl disable shairport-sync
-cp usr/local/bin/shairport-start /usr/local/bin/shairport-start
-chmod +x /usr/local/bin/shairport-start
-sed -i -e "\$i \su ${UserName} - -c /usr/local/bin/shairport-start&\n" /etc/rc.local
-usermod -a -G pulse shairport-sync
-usermod -a -G lp shairport-sync
+excmd systemctl disable shairport-sync
+excmd cp usr/local/bin/shairport-start /usr/local/bin/shairport-start
+excmd chmod +x /usr/local/bin/shairport-start
+excmd sed -i -e "\$i \su ${UserName} - -c /usr/local/bin/shairport-start&\n" /etc/rc.local
+excmd usermod -a -G pulse shairport-sync
+excmd usermod -a -G lp shairport-sync
 exit 0
