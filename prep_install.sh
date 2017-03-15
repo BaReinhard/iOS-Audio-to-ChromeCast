fc4068dfdf83146d5e403483f81f7df4c9290b06
Brett Reinhard <brett@reinhards.us>
2017-03-15 06:34:24 -0700

    Update prep_install.sh
    backs up old asound.conf

--- prep_install.sh
+++ prep_install.sh
@@ -2,8 +2,10 @@
 
 apt-add-repository ppa:qos/pulseaudio-dlna
 apt-add-repository ppa:dantheperson/shairplay-sync
-apt-get update
-apt-get upgrade
+apt-get update -y
+apt-get upgrade -y
+echo "" | tee -a /etc/asound.conf
+cp /etc/asound.conf /etc/asound.conf.old
 cp etc/asound.conf /etc/asound.conf
 
 exit 0
