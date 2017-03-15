0eaa4631cae07b71fde0c7fde86eb67727bef1b7
Brett Reinhard <brett@reinhards.us>
2017-03-15 07:26:11 -0700

    Update install.sh
    added commentary

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 log="./install.log"
+
+# Create the log file if it doesn't already exist
 echo "" | tee -a $log
+
+# Make all files executable if they aren't already
 chmod +x ./*
+
+# Prompts user for User Name of the user they are using to login (needed due to script being run as root)
 UserNameAnswer="UserNameAnswer"
 while [ $UserNameAnswer != "y" ];
 do
@@ -9,6 +15,8 @@ do
   echo "User name is : ${UserName}"
   read -p "Is this correct? (y/n) : " UserNameAnswer
 done
+
+# Prompts user for AirPlay speaker name, to let them use a custom name for the speaker
 AirPlayNameAnswer="AirPlayNameAnswer"
 while [ $AirPlayNameAnswer != "y" ];
 do
@@ -16,6 +24,7 @@ do
   echo "AirPlay Speaker name is : ${AirPlayName}"
   read -p "Is this correct? (y/n) : " AirPlayNameAnswer
 done
+
 #============================== Error Handler ==========================
 function excmd {
   echo "==================> Executing: $*"
@@ -25,6 +34,8 @@ function excmd {
   fi
 }
 #============================== End Error Handler ======================
+
+
 echo " =========================== Starting Prep Work ======================="
 excmd prep_install.sh | tee -a $log
 echo " ======================================================================"
@@ -35,7 +46,7 @@ echo " ====================== Installing Shairport-Sync ====================="
 { echo "${UserName}"; echo "${AirPlayName}";} | excmd shairport-sync_install.sh | tee -a $log
 echo " ======================================================================"
 
-# Prompts for Reboot
+# Prompts the user to Reboot, gives the option in case the script failed inside an important step in a sub file
 REBOOT="REBOOT"
 while [ $REBOOT != "y" ] && [ $REBOOT != "n" ];
 do
