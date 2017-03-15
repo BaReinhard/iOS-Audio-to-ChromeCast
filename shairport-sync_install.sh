6d0176e95d9060d62f01d467568f667b233ff7a5
Brett Reinhard <brett@reinhards.us>
2017-03-15 07:47:32 -0700

    Update shairport-sync_install.sh

--- shairport-sync_install.sh
+++ shairport-sync_install.sh
@@ -13,13 +13,25 @@ function excmd {
 }
 #============================== End Error Handler ======================
 
-
+# Installs shairport-sync
 apt-get install shairport-sync
+
+# Disables shairport-sync as a system daemon
 excmd systemctl disable shairport-sync
+
+# Copies the new shairport-start script which will handle starting shairport
 excmd cp usr/local/bin/shairport-start /usr/local/bin/shairport-start
+
+# Makes the new script executable
 excmd chmod +x /usr/local/bin/shairport-start
+
+# Inserts a line to /etc/rc.local to allow shairport-sync to execute as a non root user
 excmd sed -i -e "\$i \su ${UserName} - -c /usr/local/bin/shairport-start&\n" /etc/rc.local
+
+# Inserts the Chosen AirPlay name into shairport-sync.conf at line 7
 excmd sed -i -e "\7i \name = "${AirPlay}";\n" /etc/shairport-sync.conf
+
+# Added shairport-sync to the pulse and lp group
 excmd usermod -a -G pulse shairport-sync
 excmd usermod -a -G lp shairport-sync
 exit 0
