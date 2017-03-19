65d4d0a810c7b9504615db2873e449ab42078e7f
Brett Reinhard <brett@reinhards.us>
2017-03-18 20:00:53 -0700

    Create jsonString.sh

---
+++ JSONFormatting/jsonString.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+counter=0
+base=1
+sinkCounter=0
+echo [
+for param in "$@"
+do
+        counter=$(($counter + $base))
+        if [ $((counter%2)) -eq 0 ];
+        then
+                echo "  \"name\":\"${param}\"},"
+
+                sinkCounter=$(($sinkCounter + $base))
+        else
+#                echo "Sink${sinkCounter}:["
+
+                echo "{ \"sink\":\"${param}\", "
+        fi
+
+done
+
+
+#echo "{\"sinkIndex\":\" $(($sinkCounter - 1))\"}"
+echo ]
