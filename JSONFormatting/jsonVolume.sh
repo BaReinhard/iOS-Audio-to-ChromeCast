60b9bf47905037bfd78a8df5dd1da46438149349
Brett Reinhard <brett@reinhards.us>
2017-03-20 07:52:42 -0700

    Create jsonVolume.sh

---
+++ JSONFormatting/jsonVolume.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#!/bin/bash
+counter=0
+base=1
+sinkCounter=0
+echo [
+for param in "$@"
+do
+        
+        
+                echo " { \"Volume\":\"${param}\"},"
+
+               
+        
+
+
+                
+        
+
+done
+echo ]
+
