3aa9e71ac888baf49014bf0bae652baa7fd94302
Brett Reinhard <brett@reinhards.us>
2017-03-20 07:49:12 -0700

    Update genSinkVol.sh

--- JSONFormatting/genSinkVol.sh
+++ JSONFormatting/genSinkVol.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pactl list sinks | grep -E Volume | grep front-left | sed 's/.*,//' | sed 's/%.*//' | sed 's/.*\/ //' | ./jsonVolume.sh
+pactl list sinks | grep -E Volume | grep front-left | sed 's/.*,//' | sed 's/%.*//' | sed 's/.*\/ //' | xargs /home/{USERNAME}/jsonVolume.sh
