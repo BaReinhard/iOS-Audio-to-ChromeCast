9f4a0952c529c87b634cc2ad71d68c4a66034d79
Brett Reinhard <brett@reinhards.us>
2017-03-23 09:41:11 -0700

    Update jsonString.sh

--- JSONFormatting/jsonString.sh
+++ JSONFormatting/jsonString.sh
@@ -2,20 +2,45 @@
 counter=0
 base=1
 sinkCounter=0
+parameters=$#
+numParameters=$((parameters/3))
+
 echo [
-for param in "$@"
+#for (( param = 1; i<=${numParameters};param+4 ))
+#do
+#       nameParam=$((param+1))
+#       volParam=$((param+2))
+#       echo "{"
+#       echo " \"sink\" : \"${(${param})}\","
+#       echo " \"name\" : \"${$nameParam}\","
+#       echo " \"volume\" : \"${$volParam}\""
+#       echo " } ,"
+#done
+
+while [ $# -gt 0 ]
 do
-        counter=$(($counter + $base))
-        if [ $((counter%2)) -eq 0 ];
+
+        id=$1
+        value=$3
+        echo "{ \"sink\" : \"$1\","
+        shift
+        echo "\"name\" : \"$1\","
+        shift
+        echo "\"volume\" : \"$1\","
+        shift
+        echo "\"value\" : \"$value\","
+        echo "\"options\" : {"
+        echo "\"id\" : \"$id\","
+        echo "\"floor\" : \"0\","
+        echo "\"ceil\" : \"100\","
+        echo "\"onChange\" : \"function(id, value){updateVol(id,value);}\"}"
+
+        if [ $# = 0 ]
         then
-                echo "  \"name\":\"${param}\"},"
-
-                sinkCounter=$(($sinkCounter + $base))
+                echo "}"
         else
-#                echo "Sink${sinkCounter}:["
-
-                echo "{ \"sink\":\"${param}\", "
+                echo "},"
         fi
-
 done
+
 echo ]
