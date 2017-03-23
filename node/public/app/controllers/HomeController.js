c4213f2a6ad87a1b640cb1a5fdee560537cf3c99
Brett Reinhard <brett@reinhards.us>
2017-03-23 09:48:56 -0700

    Update HomeController.js

--- node/public/app/controllers/HomeController.js
+++ node/public/app/controllers/HomeController.js
@@ -1,13 +1,30 @@
-multiRoomApp.controller('HomeController',['$scope', 'buttonService','$http',function($scope,buttonService,$http){
+multiRoomApp.controller('HomeController',['$scope', 'buttonService', '$http' ,'$timeout', function($scope,buttonService,$http,$timeout){
+
+        var vm = this;
+
         $scope.buttons = {};
 
         $scope.buttons = buttonService;
-        
+
+        $scope.currentVolume = 100;
+        $scope.currentSink = 0;
+        vm.val = 100;
+        $scope.sliderChange= function(sliderId,sliderVal){
+                volume = {volume: sliderVal};
+                $http.post('/changeVolume',volume);
+};
+
         $scope.moveSink = function(sink){
-                       $http.post('/moveSinks', sink);
-                
+                console.log(vm.val);
+                console.log($scope.currentVolume);
+                console.log($scope.currentSink);
+                console.log(sink.volume);
+                sink.volume =  vm.val;
+                console.log(sink.volume);
+                $scope.currentSink = sink.sink;
+                $http.post('/moveSink', sink);
+
+
         }
 
-
 }]);
-
