55d6d89d43dbd1a14431c2d16f7558029cdd2eb7
Brett Reinhard <brett@reinhards.us>
2017-03-23 09:49:24 -0700

    Update buttonService.js

--- node/public/app/services/buttonService.js
+++ node/public/app/services/buttonService.js
@@ -1,10 +1,17 @@
 multiRoomApp.factory('buttonService',['$http',function($http){
         var fact = {};
+        $http.get('/refreshSinks');
         $http.get('app/sinks.json').then(function(response){
                 fact.responseData = response.data;
                 console.log(fact.responseData);
-
+                console.log(fact.responseData.volume);
         });
+
         return fact;
         }]);
+
+
+
+
+
 
