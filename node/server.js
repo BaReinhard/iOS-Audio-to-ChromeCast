405223d408952d0fb6b03d498a9e372e86bdea0c
Brett Reinhard <brett@reinhards.us>
2017-03-23 09:43:36 -0700

    Update server.js

--- node/server.js
+++ node/server.js
@@ -9,22 +9,39 @@ var holder = "";
 function moveSink(index){
         exec("/usr/local/bin/changesink "+index);
 }
+var jsonString;
+function refreshSinks(){
 
+        exec('~/executeJson.sh');
+}
+//refreshSinks();
 app.use(express.static(__dirname + '/public'));         // set the static files location /public/img will be /img for users
-//app.use(app.router);
-//app.use(express.static(__dirname + "/../public"));
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser());                                          // pull information from html in POST
 app.use(methodOverride());                                      // simulate DELETE and PUT
+app.get('app/sinks.json', function(req, res){
+        console.log(req);
 
+});
+app.get('/refreshSinks',function(req,res){
 
+        refreshSinks();
 
+        res.status(200)
+        res.end();
+        return 1;
+});
+app.post('/changeVolume', function(req,res){
+        volume = req.body.volume;
+        exec("/home/brett/changeVolume.sh "+volume);
+        console.log(req.body.volume);
+});
 app.post('/moveSink',  function(req, res){
-    // is called when /performbatch is requested from any client
+    // is called when /movesink is requested from any client
         moveSink(parseInt(req.body.sink));
         //exec("/usr/locl/bin/changesink", parseInt(req.body.sink));
         console.log(req.body.sink);
-    // ... call the function that executes the batch file from your node app
+    // ... call the function that executes the shell script from your node app
 });
 app.listen(2221);
 console.log('Simple static server listening on port 2221');
