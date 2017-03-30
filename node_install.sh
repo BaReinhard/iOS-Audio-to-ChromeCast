7d134f3e37384fcbb5722c674dbd59b791a22e67
Brett Reinhard <brett@reinhards.us>
2017-03-29 19:28:19 -0700

    Create node_install.sh

---
+++ node_install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+if [ $SUDO_USER ]; then user=$SUDO_USER; else user=`whoami`; fi
+#============================== Error Handler ==========================
+function excmd {
+  echo "==================> Executing: $*"
+  if ! $*; then
+    echo "Exiting script due to error from: $*"
+    exit 1
+  fi
+}
+#============================== End Error Handler ======================
+
+# Install nodejs
+apt-get install nodejs -y
+
+
+# Install Node Package Manager
+apt-get install npm -y
+
+# Install Express
+npm install express
+
+# Install Morgan
+npm install morgan
+
+# Install Body-Parser
+npm install body-parser
+
+# Install method-override
+npm install method-override
+
+# Add nodejs startup command to .profile
+echo "nodejs /home/${user}/projects/node/server.js" >> "/home/${user}/.profile
+
+# Install Angularjs-slider
+npm install angularjs-slider
+
+# Move Necessary files to home directory
+cp JSONFormatting/* "/home/${user}/"
+
+# Make files executable
+chmod +x "/home/${user}/"*
+
+# Make projects folder
+mkdir "/home/${user}/projects"
+
+# Move node contents to projects folder
+cp node -R "/home/${user}/projects/"
+
+# Add speaker.png
+wget https://cdn4.iconfinder.com/data/icons/proglyphs-multimedia/512/Speaker-512.png -P "/home/${user}/projects/node/public/app/img/"
+
+# Name Speaker-512.png to speaker.png
+mv "/home/${user}/projects/node/public/app/img/Speaker-512.png" "/home/${user}/projects/node/public/app/img/speaker.png"
