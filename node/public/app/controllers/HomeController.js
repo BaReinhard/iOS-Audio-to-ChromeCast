299efb45ac352eea1a8199fab9828b550b4da4de
Brett Reinhard <brettreinhard@gmail.com>
2017-11-23 08:35:00 -0800

    Updated Volume Slider and Refresh Sinks Fix
    Fixed the functionality of refresh sinks as well as insured that the volume slider is fixed and changes the sinks volume

--- node/public/app/controllers/HomeController.js
+++ node/public/app/controllers/HomeController.js
@@ -1,41 +1,55 @@
-multiRoomApp.controller('HomeController',['$scope','buttonService', '$http' ,'$timeout', function($scope,buttonService,$http,$timeout){
-    $scope.getButtons = function () {
-        buttonService.get().then(function (buttons) {
-            $scope.buttons = buttons.data;
-            $scope.val = parseInt($scope.buttons[0].volume);
-        });
-    }
-    // Get Buttons/Sinks
-    $scope.getButtons();
-   $scope.currentVolume = 100;
-            $scope.currentSink = 0;
-            $scope.sliderEnd = function () {
-                $scope.buttons[$scope.currentSink].volume = $scope.val;
-                $http.post('/changeVolume', {volume: $scope.val});
+multiRoomApp.controller('HomeController', [
+    '$scope',
+    'buttonService',
+    '$http',
+    '$timeout',
+    function($scope, buttonService, $http, $timeout) {
+        $scope.getButtons = function() {
+            buttonService.get().then(function(buttons) {
+                $scope.buttons = buttons.data;
+                console.warn(buttons.data);
+                console.error($scope.buttons);
+                $scope.val = parseInt($scope.buttons[0].volume);
+            });
+        };
+        // Get Buttons/Sinks
+        $scope.getButtons();
+        $scope.currentVolume = 100;
+        $scope.currentSink = 0;
+        $scope.sliderEnd = function() {
+            $scope.buttons[$scope.currentSink].volume = $scope.val;
+            $http.post('/changeVolume', { volume: $scope.val });
+        };
+
+        // Called on to Refresh Buttons
+        $scope.refreshSinks = function() {
+            $http.get('/refreshSinks').then(function(res) {
+                console.log(res);
+                $scope.buttons = res.data;
+            });
+        };
+
+        $scope.moveSink = function(index) {
+            $scope.val = parseInt($scope.buttons[index].volume);
+            $scope.currentSink = $scope.buttons[index].sink;
+            $http.post('/moveSink', { sink: $scope.currentSink });
+            for (var i = 0; i < $scope.buttons.length; i++) {
+                if (i == index) {
+                    $('i')
+                        .eq(i)
+                        .addClass('on');
+                    $('i')
+                        .eq(i)
+                        .removeClass('off');
+                } else {
+                    $('i')
+                        .eq(i)
+                        .addClass('off');
+                    $('i')
+                        .eq(i)
+                        .removeClass('on');
+                }
             }
-
-    // Called on to Refresh Buttons
-    $scope.refreshSinks = function(){
-        $http.get('/refreshSinks').then(function () {
-            $scope.getButtons();
-        });        
-    }
-
-    $scope.moveSink = function (index) {
-        $scope.val = parseInt($scope.buttons[index].volume);
-        $scope.currentSink = $scope.buttons[index].sink;
-        $http.post('/moveSink', {sink: $scope.currentSink});
-	for(var i = 0;i<$scope.buttons.length;i++){
-		if(i==index){
-			$('i').eq(i).addClass('on');
-			$('i').eq(i).removeClass('off');
-		}else{
-			$('i').eq(i).addClass('off');
-			$('i').eq(i).removeClass('on');
-		}
-	}
-
-    }
-
-
-}]);
+        };
+    },
+]);
--- node/server.js
+++ node/server.js
@@ -1,43 +1,53 @@
-var express        = require('express');
-var morgan         = require('morgan');
-var bodyParser     = require('body-parser');
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var app            = express();
-var exec           = require("child_process").exec;
-var execFile       = require("child_process").execFile;
+var app = express();
+var exec = require('child_process').exec;
+var execFile = require('child_process').execFile;
 
-function moveSink(index){
-  exec("/usr/local/bin/changesink "+index);
+function moveSink(index) {
+    exec('/usr/local/bin/changesink ' + index);
 }
-function changeVolume(volume){
-  exec("/usr/local/bin/changeVolume.sh "+volume);
+function changeVolume(volume) {
+    exec('/usr/local/bin/changeVolume.sh ' + volume);
 }
-function refreshSinks(){
+function os_func() {
+    this.execCommand = function(cmd, callback) {
+        exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
 
-        exec('/usr/local/bin/executeJson.sh');
+            callback(stdout);
+        });
+    };
 }
-refreshSinks();
-app.use(express.static(__dirname + '/public'));         // set the static files location /public/img will be /img for users
+
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 // Uncomment to Debug
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser());                                          // pull information from html in POST
-app.use(methodOverride());                                      // simulate DELETE and PUT
-app.get('/refreshSinks',function(req,res){
-	// Not Implemented Yet
-  refreshSinks();
-  res.status(200);
-	// Need to add reading of .json file to update sinks
-  res.end();
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser()); // pull information from html in POST
+app.use(methodOverride()); // simulate DELETE and PUT
+app.get('/refreshSinks', function(req, res) {
+    // Not Implemented Yet
+    var os = new os_func();
+    os.execCommand('/usr/local/bin/executeJson.sh', function(returnValue) {
+        var sinksFile = JSON.parse(require('fs').readFileSync(__dirname + '/public/app/sinks.json', 'utf8'));
+        res.status(200);
+        res.json(sinksFile);
+    });
 });
-app.post('/changeVolume', function(req,res){
-  changeVolume(req.body.volume);
-	res.status(200);
-	res.end();
+app.post('/changeVolume', function(req, res) {
+    changeVolume(req.body.volume);
+    res.status(200);
+    res.end();
 });
-app.post('/moveSink',  function(req, res){
-  moveSink(parseInt(req.body.sink));
-	res.status(200);
-	res.end();
+app.post('/moveSink', function(req, res) {
+    moveSink(parseInt(req.body.sink));
+    res.status(200);
+    res.end();
 });
 app.listen(2221);
 console.log('Simple static server listening on port 2221');
