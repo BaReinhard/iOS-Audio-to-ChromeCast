320cc95fe088aab6241b7733fe150b71c0894d20
Brett Reinhard <brettreinhard@gmail.com>
2017-11-24 12:23:53 -0800

    Update pa_fix.sh

--- pa_fix.sh
+++ pa_fix.sh
@@ -4,6 +4,7 @@ pulseaudio -k
 if [ $SUDO_USER ]; then user=$SUDO_USER; else user=`whoami`; fi
 export XAUTHORITY=/home/$user/.Xauthority
 export DISPLAY=':0'
+shairport-sync -o alsa -c pulse -c /etc/shairport-sync.conf&
 pulseaudio -nF /etc/pulse/default.pa&
 ## Uncomment to have pulseaudio-dlna start correctly
 pulseaudio-dlna&
