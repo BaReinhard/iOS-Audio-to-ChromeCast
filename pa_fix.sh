fa8fe00ed73485257e90ec5243a5e380fcaa5378
Brett Reinhard <brettreinhard@gmail.com>
2017-11-25 09:13:45 -0800

    added node server to startup

--- pa_fix.sh
+++ pa_fix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pulseaudio -k
+pkill pulse | echo "pulse was not running"
 if [ $SUDO_USER ]; then user=$SUDO_USER; else user=`whoami`; fi
 export XAUTHORITY=/home/$user/.Xauthority
 export DISPLAY=':0'
@@ -8,3 +8,4 @@ shairport-sync -o alsa -c pulse -c /etc/shairport-sync.conf&
 pulseaudio -nF /etc/pulse/default.pa&
 ## Uncomment to have pulseaudio-dlna start correctly
 pulseaudio-dlna&
+nodejs ~/projects/node/server.js&
