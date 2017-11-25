bd8dfac5aa48976515b54c08a58cd84b21f5ef34
Brett Reinhard <brettreinhard@gmail.com>
2017-11-24 16:45:24 -0800

    Fix path

--- JSONFormatting/genSinks.sh
+++ JSONFormatting/genSinks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pactl list sinks | grep -E 'Sink|Name|Volume: front-left' | sed 's/.*,//' | sed 's/%.*//' | sed 's/.*\ //' | sed 's/.*#//' | sed 's/.*: //' | xargs ~/jsonString.sh
+pactl list sinks | grep -E 'Sink|Name|Volume: front-left' | sed 's/.*,//' | sed 's/%.*//' | sed 's/.*\ //' | sed 's/.*#//' | sed 's/.*: //' | xargs /usr/local/bin/jsonString.sh
