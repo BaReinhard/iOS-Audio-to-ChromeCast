9a038ba041f5e59ffbdc5c125f44c18690c5f4bb
Brett Reinhard <brettreinhard@gmail.com>
2017-11-27 11:39:48 -0800

    added cmus for url streaming

---
+++ url_stream_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#============================== Error Handler ==========================
+function excmd {
+  echo "==================> Executing: $*"
+  if ! $*; then
+    echo "Exiting script due to error from: $*"
+    exit 1
+  fi
+}
+#============================== End Error Handler ======================
+
+excmd apt-get install cmus -y
