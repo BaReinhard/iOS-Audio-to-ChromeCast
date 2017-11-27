a116626a88573ecd6b3d0d98a24747f2d172c568
Brett Reinhard <brettreinhard@gmail.com>
2017-11-27 13:30:36 -0800

    replaced multiple echo statements with cat EOT

--- url_stream_install.sh
+++ url_stream_install.sh
@@ -12,13 +12,14 @@ function excmd {
 
 excmd apt-get install cmus -y
 mkdir ~/.config/cmus
-echo "set mixer.alsa.channel=" >> ~/.config/cmus/rc
-echo "set mixer.alsa.device=" >> ~/.config/cmus/rc
-echo "set mixer.oss.channel=PCM" >> ~/.config/cmus/rc
-echo "set mixer.oss.device=" >> ~/.config/cmus/rc
-echo "set mixer.pulse.restore_volume=1" >> ~/.config/cmus/rc
-echo "set output_plugin=pulse" >> ~/.config/cmus/rc
-echo "set softvol=false" >> ~/.config/cmus/rc
-
+cat << EOT >> ~/.config/cmus/rc
+set mixer.alsa.channel=
+set mixer.alsa.device=
+set mixer.oss.channel=PCM
+set mixer.oss.device=
+set mixer.pulse.restore_volume=1
+set output_plugin=pulse
+set softvol=false
+EOT
 
  
