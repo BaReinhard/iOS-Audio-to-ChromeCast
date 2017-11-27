674bd729c0639f7ff72952c4a25d7653fb04da09
Brett Reinhard <brettreinhard@gmail.com>
2017-11-27 13:24:05 -0800

    add cmus configuration

--- url_stream_install.sh
+++ url_stream_install.sh
@@ -11,3 +11,14 @@ function excmd {
 #============================== End Error Handler ======================
 
 excmd apt-get install cmus -y
+mkdir ~/.config/cmus
+echo "set mixer.alsa.channel=" >> ~/.config/cmus/rc
+echo "set mixer.alsa.device=" >> ~/.config/cmus/rc
+echo "set mixer.oss.channel=PCM" >> ~/.config/cmus/rc
+echo "set mixer.oss.device=" >> ~/.config/cmus/rc
+echo "set mixer.pulse.restore_volume=1" >> ~/.config/cmus/rc
+echo "set output_plugin=pulse" >> ~/.config/cmus/rc
+echo "set softvol=false" >> ~/.config/cmus/rc
+
+
+ 
